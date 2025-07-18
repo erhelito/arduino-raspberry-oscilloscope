7a15169afbacb2b04b67d6d9c14d59722c05fb29
erhelito <erhelito@yahoo.fr>
2025-07-18 11:02:57 +0200

    adapt time axis

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-###############################
-###############################
-# change the way x axis is displayed / computed
-
 from modules import *
 import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import numpy as np
+import time
 
 ### Configuration ###
-SERIAL_PORT = '/dev/ttyACM1'
+SERIAL_PORT = '/dev/ttyACM0'
 BAUDRATE = 9600
 
-time_scale = 3 # Time scale for the x-axis in seconds
+time_scale = 2 # Time scale for the x-axis in seconds
 frames_amount = 200  # Number of frames to display
 anim_delay = time_scale * 1000 / frames_amount  # Animation update interval in milliseconds
 
@@ -24,6 +20,7 @@ ser = serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1)
 
 data1 = []
 data2 = []
+time_list = []
 max_points = frames_amount
 
 fig, ax = plt.subplots()
@@ -45,17 +42,22 @@ def update(frame):
     d1,d2 = retrieve_data(ser)
 
     d1,d2 = convert_to_voltage(d1, d2, vref=ref_voltage, adc_resolution=resolution)
-
+    
+    current_time = time.time()
+    time_list.append(current_time)
     data1.append(d1)
     data2.append(d2)
 
     if len(data1) > max_points :
         data1.pop(0)
         data2.pop(0)
+        time_list.pop(0)
     
-    time = [i*(time_scale/len(data1)) for i in range(len(data1))]  # Generate time values 
-    line1.set_data(time, data1)
-    line2.set_data(time, data2)
+    time_list_adapted = [t-time_list[0] for t in time_list]
+
+    print(time_list_adapted)
+    line1.set_data(time_list_adapted, data1)
+    line2.set_data(time_list_adapted, data2)
 
     return line1, line2
 
